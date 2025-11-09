28e16ef684e96b8e24354cf071a2a76356f9e7b3
Mustafa <mustafarifoglu@gmail.com>
2025-11-09 10:19:10 +0000

    VFX Shader bug fixed

--- Assets/Scripts/Entity/Entity_VFX.cs
+++ Assets/Scripts/Entity/Entity_VFX.cs
@@ -13,44 +13,33 @@ public class Entity_VFX : MonoBehaviour
     private Coroutine onDamageVfxCoroutine;
 
     [Header("Doing Damage VFX")]
-    [SerializeField] private Color hitVfxColor = Color.white;
+    [SerializeField] private Color hitVfxColor = Color.red;
     [SerializeField] private GameObject hitVfx;
     [SerializeField] private GameObject critHitVfx;
-
 
     [Header("Counter Attack VFX")]
     [SerializeField] private GameObject counterAttackVFX; // for counter attack effects
 
+    [Header("Element VFX")]
+    [SerializeField] private Color chillVfx = Color.cyan;
+    private Color originalHitVfxColor;
+
     private void Awake()
     {
-        sr = GetComponentInChildren<SpriteRenderer>();
-        if (sr != null)
-            originalMaterial = sr.material;
-
         entity = GetComponent<Entity>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        originalMaterial = sr.material;
+        originalHitVfxColor = hitVfxColor;
     }
 
     public void CreateOnHitVFX(Transform target, bool isCrit)
     {
         GameObject hitPrefab = isCrit ? critHitVfx : hitVfx;
-        if (hitPrefab == null)
-        {
-            Debug.LogWarning($"[{gameObject.name}] Hit VFX prefab is not assigned!");
+        if (hitPrefab == null) 
             return;
-        }
-
+        
         GameObject vfx = Instantiate(hitPrefab, target.position, Quaternion.identity);
-
-        // Check if the instantiated VFX has a SpriteRenderer before trying to access it
-        SpriteRenderer vfxRenderer = vfx.GetComponent<SpriteRenderer>();
-        if (vfxRenderer != null)
-        {
-            vfxRenderer.color = hitVfxColor;
-        }
-        else
-        {
-            Debug.LogWarning($"[{gameObject.name}] VFX prefab '{hitPrefab.name}' doesn't have a SpriteRenderer component!");
-        }
+        vfx.GetComponentInChildren<SpriteRenderer>().color = hitVfxColor;
 
         if (entity != null && entity.facingDir == -1 && isCrit)
             vfx.transform.Rotate(0f, 180f, 0);
@@ -66,18 +55,9 @@ public class Entity_VFX : MonoBehaviour
 
     private IEnumerator OnDamageVfxCo()
     {
-        if (sr == null)
-            yield break;
-
-        Color originalColor = sr.color;
-        sr.color = Color.paleVioletRed;
-
+        sr.material = onDamageMaterial;
         yield return new WaitForSeconds(onDamageVfxDuration);
-        sr.color = originalColor;
-
-        //sr.material = onDamageMaterial;
-        //sr.material = originalMaterial;
-        //yield return new WaitForSeconds(onDamageVfxDuration);
+        sr.material = originalMaterial;
     }
 
     public void CreateCounterAttackVFX(Transform target)
